d50771f382f2bba4d72e2ef32da6cc5de5f9116e
Maicon Gilton <maicongilton@gmail.com>
2021-08-12 16:32:15 -0300

    add not found page

---
+++ src/app/pages/404/NotFound.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { initRoute } from '../../Router';
+
+const NotFound = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexDirection: 'column'
+        }}>
+            <h1>404 - Pagina não encontrada!</h1>
+
+            <Link to={initRoute}>
+                Voltar
+            </Link>
+        </div>
+    )
+}
+export default NotFound
